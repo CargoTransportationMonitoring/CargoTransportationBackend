91ba4cad6a23905267d0fa6421484062273de0e7
nikitaivanov <iwaa0303@mail.ru>
2025-02-13 14:41:14 +0300

    added security utils

---
+++ CargoCore/src/main/kotlin/org/example/cargotransporationmonitoring/util/SecurityUtils.kt
@@ -0,0 +1,20 @@
+package org.example.cargotransporationmonitoring.util
+
+import org.example.cargotransporationmonitoring.util.ErrorMessages.JWT_TOKEN_NOT_FOUND
+import org.example.cargotransporationmonitoring.util.ErrorMessages.SUBJECT_NOT_FOUND_IN_JWT
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.oauth2.jwt.Jwt
+
+object SecurityUtils {
+
+    private const val SUB_CLAIM = "sub"
+
+    fun getCurrentUserId(): String {
+        val authentication = SecurityContextHolder.getContext().authentication
+        val jwt = authentication.principal as? Jwt
+            ?: throw IllegalArgumentException(JWT_TOKEN_NOT_FOUND)
+
+        return jwt.claims[SUB_CLAIM] as? String
+            ?: throw IllegalArgumentException(SUBJECT_NOT_FOUND_IN_JWT)
+    }
+}
